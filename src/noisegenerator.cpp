23b1349e444c1ab9c51766b596e78724affed4a1
veiyas <david.rk97@gmail.com>
2021-12-21 18:47:43 +0100

    Now the noise generator is truly index safe. Possibly inefficient too with all the extra safety

--- src/noisegenerator.cpp
+++ src/noisegenerator.cpp
@@ -1,23 +1,23 @@
 #include "noisegenerator.hpp"
 
-NoiseGenerator::NoiseGenerator()
-{
-        _permutation = {
-            151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
-            8,99,37,240,21,10,23,190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,
-            35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,
-            134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,
-            55,46,245,40,244,102,143,54, 65,25,63,161,1,216,80,73,209,76,132,187,208, 89,
-            18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,
-            250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,
-            189,28,42,223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167,
-            43,172,9,129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,
-            97,228,251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,
-            107,49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-            138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 };
-
-        _permutation.insert(_permutation.end(), _permutation.begin(), _permutation.end());
-}
+//NoiseGenerator::NoiseGenerator()
+//{
+//        _permutation = {
+//            151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
+//            8,99,37,240,21,10,23,190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,
+//            35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,
+//            134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,
+//            55,46,245,40,244,102,143,54, 65,25,63,161,1,216,80,73,209,76,132,187,208, 89,
+//            18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,
+//            250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,
+//            189,28,42,223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167,
+//            43,172,9,129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,
+//            97,228,251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,
+//            107,49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+//            138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 };
+//
+//        _permutation.insert(_permutation.end(), _permutation.begin(), _permutation.end());
+//}
 
 NoiseGenerator::NoiseGenerator(unsigned seed)
 {
@@ -32,6 +32,14 @@ NoiseGenerator::NoiseGenerator(unsigned seed)
 
     _permutation.insert(_permutation.begin(), _permutation.end(), _permutation.end());
 }
+
+void NoiseGenerator::updateSeed(unsigned seed)
+{
+	//Randomly generated seed to initialize random engine
+	std::default_random_engine engine(seed);
+	std::shuffle(_permutation.begin(), _permutation.end(), engine);
+}
+
 
 double NoiseGenerator::lerp(double w, double a, double b)
 {
@@ -74,17 +82,20 @@ double NoiseGenerator::perlinNoise(double x, double z, double y)
 	double v = fade(y);
 	double w = fade(z);
 
+	size_t maxSize = _permutation.size();
+
 	// Hash coordinates of the 8 cube corners
-	int A = (_permutation[X] + Y) % 256;
-	int AA = (_permutation[A] + Z) % 256;
-	int AB = (_permutation[A + 1] + Z) % 256;
-	int B = (_permutation[X + 1] + Y) % 256;
-	int BA = (_permutation[B] + Z) % 256;
-	int BB = (_permutation[B + 1] + Z) % 256;
+	int A = (_permutation[X % maxSize] + Y) % maxSize - 1;
+	int AA = (_permutation[A % maxSize] + Z) % maxSize - 1;
+	int AB = (_permutation[(A + 1) % maxSize] + Z) % maxSize - 1;
+	int B = (_permutation[(X + 1) % maxSize] + Y) % maxSize - 1;
+	int BA = (_permutation[B % maxSize] + Z) % maxSize - 1;
+	int BB = (_permutation[(B + 1) % maxSize] + Z) % maxSize - 1;
 
 	// Add blended results from 8 corners of cube
-	double res = lerp(w, lerp(v, lerp(u, grad(_permutation[AA], x, y, z), grad(_permutation[BA], x - 1, y, z)), lerp(u, grad(_permutation[AB], x, y - 1, z), grad(_permutation[BB], x - 1, y - 1, z))), lerp(v, lerp(u, grad(_permutation[AA + 1], x, y, z - 1), grad(_permutation[BA + 1], x - 1, y, z - 1)), lerp(u, grad(_permutation[AB + 1], x, y - 1, z - 1), grad(_permutation[BB + 1], x - 1, y - 1, z - 1))));
+	double res = lerp(w, lerp(v, lerp(u, grad(_permutation[AA % maxSize], x, y, z), grad(_permutation[BA % maxSize], x - 1, y, z)), lerp(u, grad(_permutation[AB % maxSize], x, y - 1, z), grad(_permutation[BB % maxSize], x - 1, y - 1, z))), lerp(v, lerp(u, grad(_permutation[(AA + 1) % maxSize], x, y, z - 1), grad(_permutation[(BA + 1) % maxSize], x - 1, y, z - 1)), lerp(u, grad(_permutation[(AB + 1) % maxSize], x, y - 1, z - 1), grad(_permutation[(BB + 1) % maxSize], x - 1, y - 1, z - 1))));
 	return (res + 1.0) / 2.0;
 }
+
 
 
